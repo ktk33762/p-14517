443b6e0d0e1ee2bb6e4cec6fd411844451172edc
ktk3376 <ktk3376@gmail.com>
2026-05-14 09:33:30 +0900

    6강, 게시물 작성

--- src/main/java/com/back/domain/post/post/repository/PostRepository.java
+++ src/main/java/com/back/domain/post/post/repository/PostRepository.java
@@ -1,7 +1,9 @@
 package com.back.domain.post.post.repository;
 
 import com.back.domain.post.post.dto.Post;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -27,4 +29,16 @@ public interface PostRepository {
             """)
 
     Post findById(int id);
+
+    @Insert("""
+            <script>
+            insert into post
+            set createDate = NOW(),
+            modifyDate = NOW(),
+            title = #{title},
+            content = #{content}
+            </script>
+            """)
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    int create(Post post);
 }
