3e64f223ccba7b5f0e6994ba03ab51e6ddbd369e
ktk3376 <ktk3376@gmail.com>
2026-05-14 15:20:36 +0900

    20강, 회원 단건 조회 - ID로 조회 테스트 케이스

---
+++ src/main/java/com/back/domain/member/member/service/MemberService.java
@@ -0,0 +1,22 @@
+package com.back.domain.member.member.service;
+
+import com.back.domain.member.member.dto.Member;
+import com.back.domain.member.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public List<Member> findAll() {
+        return memberRepository.findAll();
+    }
+
+    public Member findById(int id) {
+        return memberRepository.findById(id);
+    }
+}
---
+++ src/test/java/com/back/domain/member/member/MemberServiceTests.java
@@ -0,0 +1,41 @@
+package com.back.domain.member.member;
+
+import com.back.domain.member.member.dto.Member;
+import com.back.domain.member.member.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@Transactional
+@ActiveProfiles("test")
+@SpringBootTest
+public class MemberServiceTests {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Test
+    @DisplayName("회원 다건 조회")
+    void t1() {
+        List<Member> members = memberService.findAll();
+
+        assertThat(members).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("회원 단건 조회 - ID로 조회")
+    void t2() {
+        Member member = memberService.findById(1);
+
+        assertThat(member.getUsername()).isEqualTo("user1");
+        assertThat(member.getName()).isEqualTo("유저1");
+        assertThat(member.getEmail()).isEqualTo("user1@test.com");
+    }
+}
